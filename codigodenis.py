f674343b44022ca058004997ad4b769bf795d248
Marguel01 <miguel007sil@gmail.com>
2025-06-25 11:04:21 -0300

    Update codigodenis.py

--- codigodenis.py
+++ codigodenis.py
@@ -75,6 +75,7 @@ def enviar_pdf_email(destinatario, chamado, caminho_pdf):
 
 # --- Inicialização ---
 st.set_page_config(page_title="Sistema de Chamados TI",page_icon="https://i.ibb.co/s9fpKvjQ/logo-helpit.png", layout="centered")
+st.image("https://i.ibb.co/6RBSr4Hq/help-t-logo.png")
 inicializar_csv()
 st.markdown("""
     <style>
